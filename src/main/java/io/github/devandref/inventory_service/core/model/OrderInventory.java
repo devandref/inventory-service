e6ca117c357e712db27b7a094f3393be9e26ed8a
devandref <yoandredepaula@gmail.com>
2024-11-10 12:48:29 -0300

    teste git

---
+++ src/main/java/io/github/devandref/inventory_service/core/model/OrderInventory.java
@@ -0,0 +1,61 @@
+package io.github.devandref.inventory_service.core.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "order_inventory")
+public class OrderInventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "inventory_id", nullable = false)
+    private Inventory inventory;
+
+    @Column(nullable = false)
+    private String orderId;
+
+    @Column(nullable = false)
+    private String transactionId;
+
+    @Column(nullable = false)
+    private Integer orderQuantity;
+
+    @Column(nullable = false)
+    private Integer oldQuantity;
+
+    @Column(nullable = false)
+    private Integer newQuantity;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        var now = LocalDateTime.now();
+        updatedAt = createdAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        var now = LocalDateTime.now();
+        updatedAt = now;
+
+    }
+
+}
